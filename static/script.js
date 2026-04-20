cc96109a67076c275b5eb04126f0087e2f1acb6c
Andrea <Baltimora.andrea@iisgalvanimi.edu.it>
2026-04-20 07:03:42 +0000

    aggiunto main py non ancora finito

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 async function controllacredenziali() {
     const username = document.getElementById('username').value;
     const pasword = document.getElementById('pasword').value;
-    if (!username || !pasword ) return alert("Scrivi un nome! e pasword");
+    if (!username || !pasword ) return alert("Scrivi un nome e una pasword");
     const res = await fetch(`/login?username=${username}&pasword=${pasword}`);
     const dati = await res.json();
     document.getElementById('Risultato').innerText = dati.messaggio;
